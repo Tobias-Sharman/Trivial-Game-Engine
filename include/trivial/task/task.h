898bd955410d3c1b2e73d180fa2bb7af4f53a09a
Tobias Sharman <tobiassharman@gmail.com>
2026-07-11 19:22:50 +0100

    Add wrapper on task functions for cleaner api

---
+++ include/trivial/task/task.h
@@ -0,0 +1,68 @@
+#ifndef TRIVIAL_TASK_TASK_H
+#define TRIVIAL_TASK_TASK_H
+
+#include <span>
+#include <utility>
+
+#include <trivial/core/assert.h>
+#include <trivial/task/task_function.h>
+#include <trivial/task/task_handle.h>
+#include <trivial/task/task_launch_options.h>
+#include <trivial/task/task_system.h>
+
+namespace trivial::task {
+
+namespace detail {
+
+// Could have put in cpp and that would be safer and more "correct" but this will guarante less overhead rather than
+// maybe not have some overhead with compiler removing wrappers and inling stuff
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+inline TaskSystem* g_activeTaskSystem = nullptr;
+
+} // namespace detail
+
+inline void setActiveTaskSystem(TaskSystem* taskSystem) noexcept {
+	detail::g_activeTaskSystem = taskSystem;
+}
+
+[[nodiscard]] inline TaskSystem& activeTaskSystem() noexcept {
+	TRIVIAL_ASSERT(detail::g_activeTaskSystem != nullptr);
+
+	return *detail::g_activeTaskSystem;
+}
+
+[[nodiscard]] inline TaskHandle launch(TaskFunction function, const TaskLaunchOptions& options = {}) noexcept {
+	return activeTaskSystem().launch(std::move(function), options);
+}
+
+[[nodiscard]] inline TaskHandle launch(TaskFunction function,
+                                       TaskHandle prerequisite,
+                                       const TaskLaunchOptions& options = {}) noexcept {
+	return activeTaskSystem().launch(std::move(function), prerequisite, options);
+}
+
+[[nodiscard]] inline TaskHandle launch(TaskFunction function,
+                                       std::span<const TaskHandle> prerequisites,
+                                       const TaskLaunchOptions& options = {}) noexcept {
+	return activeTaskSystem().launch(std::move(function), prerequisites, options);
+}
+
+inline void wait(TaskHandle task) noexcept {
+	activeTaskSystem().wait(task);
+}
+
+inline void wait(std::span<const TaskHandle> tasks) noexcept {
+	activeTaskSystem().wait(tasks);
+}
+
+[[nodiscard]] inline bool isComplete(TaskHandle task) noexcept {
+	return activeTaskSystem().isComplete(task);
+}
+
+[[nodiscard]] inline TaskReleaseResult release(TaskHandle task) noexcept {
+	return activeTaskSystem().release(task);
+}
+
+} // namespace trivial::task
+
+#endif // TRIVIAL_TASK_TASK_H
--- tests/task/task_system.cpp
+++ tests/task/task_system.cpp
@@ -1,178 +1,176 @@
-#include <trivial/task/task_system.h>
-
 #include <array>
 #include <span>
 #include <vector>
 
 #include <gtest/gtest.h>
 
-#include <trivial/task/task_function.h>
-#include <trivial/task/task_handle.h>
-#include <trivial/task/task_launch_options.h>
+#include <trivial/task/task.h>
 
 namespace trivial::task {
 
 namespace {
 
-TEST(TaskSystemTests, LaunchesTaskWithoutPrerequisites) {
-	TaskSystem taskSystem;
+TaskSystem g_taskSystem;
 
+class TaskSystemTestSetup {
+public:
+	TaskSystemTestSetup() noexcept { setActiveTaskSystem(&g_taskSystem); }
+
+	~TaskSystemTestSetup() noexcept { setActiveTaskSystem(nullptr); }
+
+	TaskSystemTestSetup(const TaskSystemTestSetup&) = delete;
+	TaskSystemTestSetup& operator=(const TaskSystemTestSetup&) = delete;
+
+	TaskSystemTestSetup(TaskSystemTestSetup&&) = delete;
+	TaskSystemTestSetup& operator=(TaskSystemTestSetup&&) = delete;
+};
+
+TaskSystemTestSetup g_taskSystemTestSetup;
+
+TEST(TaskSystemTests, LaunchesTaskWithoutPrerequisites) {
 	bool executed = false;
 
-	const TaskHandle kTask = taskSystem.launch(TaskFunction{[&executed]() noexcept {
+	const TaskHandle kTask = launch(TaskFunction{[&executed]() noexcept {
 		executed = true;
 	}});
 
 	EXPECT_TRUE(kTask.isValid());
 	EXPECT_FALSE(executed);
-	EXPECT_FALSE(taskSystem.isComplete(kTask));
+	EXPECT_FALSE(isComplete(kTask));
 
-	taskSystem.wait(kTask);
+	wait(kTask);
 
 	EXPECT_TRUE(executed);
-	EXPECT_TRUE(taskSystem.isComplete(kTask));
+	EXPECT_TRUE(isComplete(kTask));
 }
 
 TEST(TaskSystemTests, WaitExecutesSinglePrerequisiteBeforeDependant) {
-	TaskSystem taskSystem;
-
 	std::vector<int> executionOrder;
 
-	const TaskHandle kPrerequisite = taskSystem.launch(TaskFunction{[&executionOrder]() noexcept {
+	const TaskHandle kPrerequisite = launch(TaskFunction{[&executionOrder]() noexcept {
 		executionOrder.push_back(1);
 	}});
 
-	const TaskHandle kDependant = taskSystem.launch(TaskFunction{[&executionOrder]() noexcept {
-		                                                executionOrder.push_back(2);
-	                                                }},
-	                                                kPrerequisite);
+	const TaskHandle kDependant = launch(TaskFunction{[&executionOrder]() noexcept {
+		                                     executionOrder.push_back(2);
+	                                     }},
+	                                     kPrerequisite);
 
 	EXPECT_TRUE(kPrerequisite.isValid());
 	EXPECT_TRUE(kDependant.isValid());
 
-	taskSystem.wait(kDependant);
+	wait(kDependant);
 
 	ASSERT_EQ(executionOrder.size(), 2UZ);
 	EXPECT_EQ(executionOrder[0], 1);
 	EXPECT_EQ(executionOrder[1], 2);
 
-	EXPECT_TRUE(taskSystem.isComplete(kPrerequisite));
-	EXPECT_TRUE(taskSystem.isComplete(kDependant));
+	EXPECT_TRUE(isComplete(kPrerequisite));
+	EXPECT_TRUE(isComplete(kDependant));
 }
 
 TEST(TaskSystemTests, WaitExecutesMultiplePrerequisitesBeforeDependant) {
-	TaskSystem taskSystem;
-
 	bool firstExecuted = false;
 	bool secondExecuted = false;
 	bool dependantExecuted = false;
 
-	const TaskHandle kFirst = taskSystem.launch(TaskFunction{[&firstExecuted]() noexcept {
+	const TaskHandle kFirst = launch(TaskFunction{[&firstExecuted]() noexcept {
 		firstExecuted = true;
 	}});
 
-	const TaskHandle kSecond = taskSystem.launch(TaskFunction{[&secondExecuted]() noexcept {
+	const TaskHandle kSecond = launch(TaskFunction{[&secondExecuted]() noexcept {
 		secondExecuted = true;
 	}});
 
 	const std::array<TaskHandle, 2> kPrerequisites{kFirst, kSecond};
 
-	const TaskHandle kDependant
-	    = taskSystem.launch(TaskFunction{[&firstExecuted, &secondExecuted, &dependantExecuted]() noexcept {
-		                        EXPECT_TRUE(firstExecuted);
-		                        EXPECT_TRUE(secondExecuted);
+	const TaskHandle kDependant = launch(TaskFunction{[&firstExecuted, &secondExecuted, &dependantExecuted]() noexcept {
+		                                     EXPECT_TRUE(firstExecuted);
+		                                     EXPECT_TRUE(secondExecuted);
 
-		                        dependantExecuted = true;
-	                        }},
-	                        std::span<const TaskHandle>{kPrerequisites});
+		                                     dependantExecuted = true;
+	                                     }},
+	                                     std::span<const TaskHandle>{kPrerequisites});
 
 	EXPECT_TRUE(kFirst.isValid());
 	EXPECT_TRUE(kSecond.isValid());
 	EXPECT_TRUE(kDependant.isValid());
 
-	taskSystem.wait(kDependant);
+	wait(kDependant);
 
 	EXPECT_TRUE(firstExecuted);
 	EXPECT_TRUE(secondExecuted);
 	EXPECT_TRUE(dependantExecuted);
 
-	EXPECT_TRUE(taskSystem.isComplete(kFirst));
-	EXPECT_TRUE(taskSystem.isComplete(kSecond));
-	EXPECT_TRUE(taskSystem.isComplete(kDependant));
+	EXPECT_TRUE(isComplete(kFirst));
+	EXPECT_TRUE(isComplete(kSecond));
+	EXPECT_TRUE(isComplete(kDependant));
 }
 
 TEST(TaskSystemTests, WaitForSpanWaitsForAllTasks) {
-	TaskSystem taskSystem;
-
 	bool firstExecuted = false;
 	bool secondExecuted = false;
 
-	const TaskHandle kFirst = taskSystem.launch(TaskFunction{[&firstExecuted]() noexcept {
+	const TaskHandle kFirst = launch(TaskFunction{[&firstExecuted]() noexcept {
 		firstExecuted = true;
 	}});
 
-	const TaskHandle kSecond = taskSystem.launch(TaskFunction{[&secondExecuted]() noexcept {
+	const TaskHandle kSecond = launch(TaskFunction{[&secondExecuted]() noexcept {
 		secondExecuted = true;
 	}});
 
 	const std::array<TaskHandle, 2> kTasks{kFirst, kSecond};
 
-	taskSystem.wait(std::span<const TaskHandle>{kTasks});
+	wait(std::span<const TaskHandle>{kTasks});
 
 	EXPECT_TRUE(firstExecuted);
 	EXPECT_TRUE(secondExecuted);
 
-	EXPECT_TRUE(taskSystem.isComplete(kFirst));
-	EXPECT_TRUE(taskSystem.isComplete(kSecond));
+	EXPECT_TRUE(isComplete(kFirst));
+	EXPECT_TRUE(isComplete(kSecond));
 }
 
 TEST(TaskSystemTests, ReleaseSucceedsAfterCompletion) {
-	TaskSystem taskSystem;
-
 	const TaskLaunchOptions kOptions{.lifetime = TaskLifetime::Manual};
 
 	bool executed = false;
 
-	const TaskHandle kTask = taskSystem.launch(TaskFunction{[&executed]() noexcept {
-		                                           executed = true;
-	                                           }},
-	                                           kOptions);
+	const TaskHandle kTask = launch(TaskFunction{[&executed]() noexcept {
+		                                executed = true;
+	                                }},
+	                                kOptions);
 
 	ASSERT_TRUE(kTask.isValid());
 
-	taskSystem.wait(kTask);
+	wait(kTask);
 
 	EXPECT_TRUE(executed);
-	EXPECT_TRUE(taskSystem.isComplete(kTask));
+	EXPECT_TRUE(isComplete(kTask));
 
-	EXPECT_EQ(taskSystem.release(kTask), TaskReleaseResult::Success);
-	EXPECT_FALSE(taskSystem.isComplete(kTask));
-	EXPECT_EQ(taskSystem.release(kTask), TaskReleaseResult::InvalidHandle);
+	EXPECT_EQ(release(kTask), TaskReleaseResult::Success);
+	EXPECT_FALSE(isComplete(kTask));
+	EXPECT_EQ(release(kTask), TaskReleaseResult::InvalidHandle);
 }
 
 TEST(TaskSystemTests, ReleaseFailsBeforeCompletion) {
-	TaskSystem taskSystem;
-
 	bool executed = false;
 
-	const TaskHandle kTask = taskSystem.launch(TaskFunction{[&executed]() noexcept {
+	const TaskHandle kTask = launch(TaskFunction{[&executed]() noexcept {
 		executed = true;
 	}});
 
 	ASSERT_TRUE(kTask.isValid());
 
-	EXPECT_EQ(taskSystem.release(kTask), TaskReleaseResult::TaskNotComplete);
+	EXPECT_EQ(release(kTask), TaskReleaseResult::TaskNotComplete);
 	EXPECT_FALSE(executed);
 
-	taskSystem.wait(kTask);
+	wait(kTask);
 
 	EXPECT_TRUE(executed);
-	EXPECT_EQ(taskSystem.release(kTask), TaskReleaseResult::Success);
+	EXPECT_EQ(release(kTask), TaskReleaseResult::Success);
 }
 
 TEST(TaskSystemTests, CriticalPriorityRunsBeforeNormalPriority) {
-	TaskSystem taskSystem;
-
 	std::vector<int> executionOrder;
 
 	TaskLaunchOptions normalOptions{};
@@ -181,22 +179,22 @@ TEST(TaskSystemTests, CriticalPriorityRunsBeforeNormalPriority) {
 	TaskLaunchOptions criticalOptions{};
 	criticalOptions.priority = TaskPriority::Critical;
 
-	const TaskHandle kNormal = taskSystem.launch(TaskFunction{[&executionOrder]() noexcept {
-		                                             executionOrder.push_back(1);
-	                                             }},
-	                                             normalOptions);
+	const TaskHandle kNormal = launch(TaskFunction{[&executionOrder]() noexcept {
+		                                  executionOrder.push_back(1);
+	                                  }},
+	                                  normalOptions);
 
-	const TaskHandle kCritical = taskSystem.launch(TaskFunction{[&executionOrder]() noexcept {
-		                                               executionOrder.push_back(2);
-	                                               }},
-	                                               criticalOptions);
+	const TaskHandle kCritical = launch(TaskFunction{[&executionOrder]() noexcept {
+		                                    executionOrder.push_back(2);
+	                                    }},
+	                                    criticalOptions);
 
 	ASSERT_TRUE(kNormal.isValid());
 	ASSERT_TRUE(kCritical.isValid());
 
 	const std::array<TaskHandle, 2> kTasks{kNormal, kCritical};
 
-	taskSystem.wait(std::span<const TaskHandle>{kTasks});
+	wait(std::span<const TaskHandle>{kTasks});
 
 	ASSERT_EQ(executionOrder.size(), 2UZ);
 	EXPECT_EQ(executionOrder[0], 2);
