532e76445706e7c1641ace9cb0a51f1e5a01adea
Tobias Sharman <tobiassharman@gmail.com>
2026-07-19 16:53:18 +0100

    Wrap frame context in trivial namespace

--- include/trivial/frame/frame_context.h
+++ include/trivial/frame/frame_context.h
@@ -3,9 +3,13 @@
 
 #include <cstdint>
 
+namespace trivial {
+
 struct FrameContext {
 	double deltaTime = 0.0;
 	std::uint64_t frameIndex = 0;
 };
 
+} // namespace trivial
+//
 #endif // TRIVIAL_FRAME_FRAME_CONTEXT_H
